c6167ba65ef8730bd0927d7df168a380291ad391
Mirzac Mihai <mihaimarzac@gmail.com>
2022-09-23 18:00:23 +0300

    Added mobile carousel

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -1,15 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  Input,
+  OnInit,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.scss']
+  styleUrls: ['./carousel.component.scss'],
 })
 export class CarouselComponent implements OnInit {
+  constructor(private renderer: Renderer2) {}
 
-  constructor() { }
+  @ViewChild('main_img', { static: false }) main_img!: ElementRef;
+  @Input() productImages: string[] = [];
 
-  ngOnInit(): void {
+  currentIndex: number = 0;
+  ngOnInit(): void {}
+
+  onBackwards() {
+    this.currentIndex--;
+
+    if (this.currentIndex === -1) {
+      this.renderer.setAttribute(
+        this.main_img.nativeElement,
+        'src',
+        this.productImages[this.productImages.length - 1].toString()
+      );
+      this.currentIndex = this.productImages.length - 1;
+    } else {
+      this.renderer.setAttribute(
+        this.main_img.nativeElement,
+        'src',
+        this.productImages[this.currentIndex].toString()
+      );
+    }
   }
 
+  onForwards() {
+    this.currentIndex++;
+
+    if (this.currentIndex > this.productImages.length - 1) {
+      this.currentIndex = 0;
+      this.renderer.setAttribute(
+        this.main_img.nativeElement,
+        'src',
+        this.productImages[this.currentIndex].toString()
+      );
+    } else {
+      this.renderer.setAttribute(
+        this.main_img.nativeElement,
+        'src',
+        this.productImages[this.currentIndex].toString()
+      );
+    }
+  }
 }
