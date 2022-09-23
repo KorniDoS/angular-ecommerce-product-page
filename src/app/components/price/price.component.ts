abadb6a8e8defe184725bdd86a0479639eeef05c
Mirzac Mihai <mihaimarzac@gmail.com>
2022-09-23 18:01:19 +0300

    Update price component

--- src/app/components/price/price.component.ts
+++ src/app/components/price/price.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product.model';
+import { StorageService } from 'src/app/services/storage.service';
 
 @Component({
   selector: 'app-price',
@@ -8,10 +9,27 @@ import { Product } from 'src/app/models/product.model';
 })
 export class PriceComponent implements OnInit {
 
-  @Input() product?: Product;
-  constructor() { }
+  @Input() product!: Product;
+
+  noOfProducts: number = 0;
+  constructor(private storageService: StorageService) { }
 
   ngOnInit(): void {
+  }
+
+  onDecreaseProductQuantity(){
+    if(this.noOfProducts === 0){
+      return;
+    } else this.noOfProducts--;
+  }
+
+  onIncreaseProductQuantity(){
+    this.noOfProducts++;
+  }
+
+  sendToCart(){
+    this.storageService.cart.next(this.product);
+    this.storageService.noOfItems.next(this.noOfProducts);
   }
 
 }
